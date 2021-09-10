651e1c511b6c26c95e651195925ef25f0b272f0e
ananthkarthik4 <86655937+ananthkarthik4@users.noreply.github.com>
2021-09-10 12:36:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var a = prompt("Enter UserName");
+var b = prompt("Enter password");
+if (a == "vaishnavi" && b == "17-09-2002") {
+	window.location.href = "start.html";
+}
+else {
+	alert("invalid details");
+	window.location.href = "index.html";
+}
